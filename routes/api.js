6d75ab49d5aea51c4e0b44164fe1e526b2be7d73
Van Huy <huyvk3110@gmail.com>
2020-01-25 13:23:37 +0700

    Done

--- routes/api.js
+++ routes/api.js
@@ -12,42 +12,69 @@ var expect = require('chai').expect;
 var MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectId;
 const MONGODB_CONNECTION_STRING = process.env.DB;
+const COLLECTION = 'books';
 //Example connection: MongoClient.connect(MONGODB_CONNECTION_STRING, function(err, db) {});
 
-module.exports = function (app) {
+module.exports = function (app, db) {
 
   app.route('/api/books')
-    .get(function (req, res){
+    .get(function (req, res) {
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+      db.collection(COLLECTION)
+        .find({})
+        .toArray()
+        .then(data => res.json(data.map(o => ({ ...o, commentcount: o.comments && o.comments.length || 0 }))))
+        .catch(error => res.json({ error }))
     })
-    
-    .post(function (req, res){
+
+    .post(function (req, res) {
       var title = req.body.title;
       //response will contain new book object including atleast _id and title
+      if (!title) return res.json({ error: 'error' });
+      db.collection(COLLECTION).insertOne({ title })
+        .then(data => res.json(data.ops[0]))
+        .catch(error => res.json({ error }))
     })
-    
-    .delete(function(req, res){
+
+    .delete(function (req, res) {
       //if successful response will be 'complete delete successful'
+      db.collection(COLLECTION)
+        .remove()
+        .then(data => res.send('complete delete successful'))
+        .catch(error => res.json({ error }))
     });
 
 
 
   app.route('/api/books/:id')
-    .get(function (req, res){
+    .get(function (req, res) {
       var bookid = req.params.id;
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
+      db.collection(COLLECTION).findOne({ _id: new ObjectId(bookid) })
+        .then(data => res.json({ ...data, comments: data.comments || [] }))
+        .catch(error => res.json({ error }));
     })
-    
-    .post(function(req, res){
+
+    .post(function (req, res) {
       var bookid = req.params.id;
       var comment = req.body.comment;
       //json res format same as .get
+      db.collection(COLLECTION).findOne({ _id: new ObjectId(bookid) })
+        .then(data => {
+          db.collection(COLLECTION).updateOne({ _id: new ObjectId(bookid) }, { ...data, comments: data.comments ? [...data.comments, comment] : [comment] })
+            .then(data => res.json(data.ops[0]))
+            .catch(error => res.json({ error }))
+        })
+        .catch(error => res.json({ error }))
     })
-    
-    .delete(function(req, res){
+
+    .delete(function (req, res) {
       var bookid = req.params.id;
       //if successful response will be 'delete successful'
+      db.collection(COLLECTION).remove({ _id: new ObjectId(bookid) })
+        .then(data => res.send('complete delete successful'))
+        .catch(error => res.json({ error }))
     });
-  
+
 };
--- server.js
+++ server.js
@@ -7,8 +7,11 @@ var cors        = require('cors');
 var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
 var runner            = require('./test-runner');
+var dotenv      = require('dotenv');
+var mongo       = require('mongodb').MongoClient;
 
 var app = express();
+dotenv.config();
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -17,27 +20,32 @@ app.use(cors({origin: '*'})); //USED FOR FCC TESTING PURPOSES ONLY!
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//Index page (static HTML)
-app.route('/')
+mongo.connect(process.env.DB, function(err,client) {
+  if(err) return console.log(err);
+
+  const db = client.db('freecodecamp');
+
+  //Index page (static HTML)
+  app.route('/')
   .get(function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
   });
 
-//For FCC testing purposes
-fccTestingRoutes(app);
+  //For FCC testing purposes
+  fccTestingRoutes(app);
 
-//Routing for API 
-apiRoutes(app);  
+  //Routing for API 
+  apiRoutes(app,db);  
     
-//404 Not Found Middleware
-app.use(function(req, res, next) {
+  //404 Not Found Middleware
+  app.use(function(req, res, next) {
   res.status(404)
     .type('text')
     .send('Not Found');
-});
+  });
 
-//Start our server and tests!
-app.listen(process.env.PORT || 3000, function () {
+  //Start our server and tests!
+  app.listen(process.env.PORT || 3000, function () {
   console.log("Listening on port " + process.env.PORT);
   if(process.env.NODE_ENV==='test') {
     console.log('Running Tests...');
@@ -51,6 +59,7 @@ app.listen(process.env.PORT || 3000, function () {
       }
     }, 1500);
   }
-});
+  });
+})
 
 module.exports = app; //for unit/functional testing
